1cc51de00be3fc1a8e4c44e82ed948282a232c64
AnnamPrasuna <AnnamPrasuna@DESKTOP-TUNPDV2>
2023-11-11 12:59:54 +0530

    Add project files.

---
+++ CFApproach/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFApproach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            StudentContext stdctx = new StudentContext();
+            Student std = new Student()
+            {
+                StudentId = 2, Name = "Mounica", Marks = 550,Department="HR2"
+
+            };
+
+            stdctx.Students.Add(std);
+            stdctx.SaveChanges();
+            Console.WriteLine("Data Added Successfuly");
+            var students = stdctx.Students.ToList();
+            foreach (var student in students) 
+            {
+                Console.WriteLine(student.StudentId+ " "+student.Name+"  "+student.Marks+"  "+student.Department);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CFApproach/StudentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFApproach
+{
+    internal class StudentContext:DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
